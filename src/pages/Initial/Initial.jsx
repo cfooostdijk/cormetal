acb661122c1846a9ceff18cc59b5ea2e9b541ae3
CrisOos <cristian.oostdijk@avenida.com>
2023-05-05 08:24:05 -0300

    Initial setup

---
+++ src/pages/Initial/Initial.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Footer from '../../Containers/Footer/Footer'
+// import Header from '../../Containers/Header/Header'
+
+const Initial = () => {
+  return (
+    <>
+    {/* <Header /> */}
+      <h1>Initial</h1>
+    {/* <Footer /> */}
+    </>
+  )
+}
+
+export default Initial
