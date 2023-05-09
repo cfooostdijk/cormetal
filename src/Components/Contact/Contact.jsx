d2e107175c3cfc68189441e576e692a1b6a4dc61
CrisOos <cristian.oostdijk@avenida.com>
2023-05-09 08:40:30 -0300

    socials fixed

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,8 +7,8 @@ const Contact = () => {
         <p className='contact-title'>Contacto</p>
         <a href="https://wa.me/5492262572060" style={{display: 'block'}}><p className='contact-paragraph'>+54 9 2262572060</p></a>
         <a href="https://wa.me/5492262572061" ><p className='contact-paragraph'>+54 9 2262572061</p></a>
-        <p className='contact-paragraph'>administracion@cormetalqqn.com.ar</p>
-        <p className='contact-paragraph'>Quequén, Buenos Aires, Argentina</p>
+        <a href="mailto:administracion@cormetalqqn.com.ar"><p className='contact-paragraph'>administracion@cormetalqqn.com.ar</p></a>
+        <a href='https://goo.gl/maps/5Ff2fTmSaXfwSAy57' ><p className='contact-paragraph'>Quequén, Buenos Aires, Argentina</p></a>
       </div>
     </div>
   )
--- src/Components/Socials/Socials.jsx
+++ src/Components/Socials/Socials.jsx
@@ -4,8 +4,8 @@ import '../styles/Socials.sass'
 const Socials = () => {
   return (
     <div className="socials-container">
-      <a href="https://www.linkedin.com/company/globalinc/" className='icon'><BsLinkedin /></a>
-      <a href="https://instagram.com/globalincok?igshid=OTJhZDVkZWE=" className='icon'><BsInstagram /></a>
+      <a href="https://www.linkedin.com/in/fernando-mario-oostdijk-146936138" className='icon'><BsLinkedin /></a>
+      <a href="https://instagram.com/fer.oostdijk" className='icon'><BsInstagram /></a>
     </div>
   )
 }
